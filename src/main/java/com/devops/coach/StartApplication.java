c0068a02c634303d7a4b65fd7e453fb549962ee0
Ebin Krishnan <ebinkrishnan012@gmail.com>
2026-09-09 18:01:44 +0530

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -19,6 +19,8 @@ public class StartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
+        String username = "admin";
+        String password = "admin123";
     }
 
 }
